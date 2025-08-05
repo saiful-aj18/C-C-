a7d183e17d54f5ea05ffe3fec5aad0ed2910a6b6
Saiful Islam <alexjamson118@gmail.com>
2025-08-05 22:45:17 +0600

    Add files via upload

---
+++ Simpal calculator.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+double num1;
+double num2;
+char op;
+
+printf("Enter num1:");
+scanf("%lf", &num1);
+printf("Enter operator:");
+scanf(" %c", &op);
+printf("Enter num2:");
+scanf("%lf", &num2);
+
+if(op == '+'){
+printf("%f",num1+num2);
+}
+else if(op == '-'){
+printf("%f",num1-num2);
+}
+else if(op == '*'){
+printf("%f",num1*num2);
+}
+else if(op == '/'){
+printf("%f",num1/num2);
+}
+else {
+printf("invalid number");
+}
+
+}
